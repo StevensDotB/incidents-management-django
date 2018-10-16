8898f7814214848e4d53888aff2c88828c54e71f
Stevens Brito <stevensbrito@gmail.com>
2018-10-16 12:37:49 -0400

    Initial models and home view

--- incidencesmanagement/core/models.py
+++ incidencesmanagement/core/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Department(models.Model):
+    """Model for departments name"""
+    name = models.CharField(max_length=50, choices=(
+        ('NSE', 'Network and System Engineering'),
+        ('SP', 'Support'),
+        ('HR', 'Human Resources'),
+        ('SD', 'Selling')
+    ))
+
+
+class UserEmployee(models.Model):
+    """User employee"""
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    department = models.ForeignKey(Department, on_delete=False)
