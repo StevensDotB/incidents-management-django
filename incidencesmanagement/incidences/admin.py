d2bd2dd1c46f2b685e3d35481cf20c849bcc57bc
Stevens Brito <stevensbrito@gmail.com>
2018-10-17 02:52:39 -0400

    Modify table view for incidences

--- incidencesmanagement/incidences/admin.py
+++ incidencesmanagement/incidences/admin.py
@@ -2,7 +2,31 @@ from django.contrib import admin
 from .models import Incidence, IncidenceLevel, IncidenceStatus
 
 
-admin.site.register(Incidence)
+class IncidenceAdmin(admin.ModelAdmin):
+    readonly_fields = ('create', 'update')
+    list_display = ('get_username',
+                    'subject',
+                    'message',
+                    'get_level',
+                    'get_status',
+                    'create',
+                    'update')
+
+    def get_username(self, instance):
+        return instance.user.username
+
+    def get_level(self, instance):
+        return instance.level.name
+
+    def get_status(self, instance):
+        return instance.status.name
+
+    get_username.short_description = 'User'
+    get_level.short_description = 'Level'
+    get_status.short_description = 'Status'
+
+
+admin.site.register(Incidence, IncidenceAdmin)
 admin.site.register(IncidenceLevel)
 admin.site.register(IncidenceStatus)
 
