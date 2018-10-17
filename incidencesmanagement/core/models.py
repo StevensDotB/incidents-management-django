0dca09e5d1d9358ff41043f2fbc3c517b6b71917
Stevens Brito <stevensbrito@gmail.com>
2018-10-17 00:45:25 -0400

    Custom tables names in core models

--- incidencesmanagement/core/models.py
+++ incidencesmanagement/core/models.py
@@ -4,15 +4,16 @@ from django.contrib.auth.models import User
 
 class Department(models.Model):
     """Model for departments name"""
-    name = models.CharField(max_length=50, choices=(
-        ('NSE', 'Network and System Engineering'),
-        ('SP', 'Support'),
-        ('HR', 'Human Resources'),
-        ('SD', 'Selling')
-    ))
+    class Meta:
+        db_table = "departments"
+
+    name = models.CharField(max_length=50)
 
 
 class UserEmployee(models.Model):
     """User employee"""
+    class Meta:
+        db_table = "users_employees"
+
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     department = models.ForeignKey(Department, on_delete=False)
