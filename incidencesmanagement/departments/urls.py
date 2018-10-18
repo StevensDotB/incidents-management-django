9b09c8fab410c3ff6ee9a220a0663e04a9177d17
Stevens Brito <stevensbrito@gmail.com>
2018-10-18 14:29:35 -0400

    Update and Delete Department

--- incidencesmanagement/departments/urls.py
+++ incidencesmanagement/departments/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import DepartmentCreate, DepartmentListView
+from .views import DepartmentCreate, DepartmentListView, DepartmentUpdate, DepartmentDelete
 
 
 departments_patterns = ([
     path('', DepartmentListView.as_view(), name="departments"),
-    path('create/', DepartmentCreate.as_view(), name="create")
+    path('create/', DepartmentCreate.as_view(), name="create"),
+    path('<int:pk>/update/', DepartmentUpdate.as_view(), name="update"),
+    path('<int:pk>/delete/', DepartmentDelete.as_view(), name="delete")
 ], 'departments')
--- incidencesmanagement/departments/views.py
+++ incidencesmanagement/departments/views.py
@@ -1,22 +1,47 @@
 from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic.edit import CreateView
 from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, DeleteView
+from django.views.generic.edit import UpdateView
 from .forms import DepartmentForm
 from .models import Department
+# ---------------------------
+# CRUD Views for Departments
+# ---------------------------
 
 
 class DepartmentListView(ListView):
+    """Display all Departments"""
     model = Department
-    paginate_by = 20
 
 
 class DepartmentCreate(CreateView):
+    """Create a new Department"""
     model = Department
     form_class = DepartmentForm
 
     def get_success_url(self, *args):
-        return reverse_lazy('departments:departments') + '?created'
+        return reverse_lazy('departments:departments') + '?action=created'
+
+
+class DepartmentUpdate(UpdateView):
+    """Update/Edit a Department"""
+    model = Department
+    form_class = DepartmentForm
+    template_name_suffix = '_update_form'
+
+    def get_success_url(self, *args):
+        return reverse_lazy('departments:departments') + '?action=updated'
+
+
+class DepartmentDelete(DeleteView):
+    """Delete a Department"""
+    model = Department
+
+    def get_success_url(self, *args):
+        return reverse_lazy('departments:departments') + '?action=deleted'
+
+
 
 
 
