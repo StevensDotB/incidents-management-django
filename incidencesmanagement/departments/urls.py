96b26cd7339659f7902cb1b8d3f99386068b7e3a
Stevens Brito <stevensbrito@gmail.com>
2018-10-18 04:16:37 -0400

    Add Department list view

--- incidencesmanagement/departments/urls.py
+++ incidencesmanagement/departments/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import DepartmentCreate
+from .views import DepartmentCreate, DepartmentListView
 
 
 departments_patterns = ([
-    path('create/', DepartmentCreate.as_view())
+    path('', DepartmentListView.as_view(), name="departments"),
+    path('create/', DepartmentCreate.as_view(), name="create")
 ], 'departments')
--- incidencesmanagement/departments/views.py
+++ incidencesmanagement/departments/views.py
@@ -1,12 +1,20 @@
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from .forms import DepartmentForm
 from .models import Department
+
+
+class DepartmentListView(ListView):
+    model = Department
+    paginate_by = 20
 
 
 class DepartmentCreate(CreateView):
     model = Department
     form_class = DepartmentForm
+    success_url = reverse_lazy('departments:departments')
 
 
 
