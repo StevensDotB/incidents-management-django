1a4fd6e5ae410a72d3e84a0c7dc2f71d3d4f1c10
Stevens Brito <stevensbrito@gmail.com>
2018-10-19 00:54:47 -0400

    Add users list view

--- incidencesmanagement/core/views.py
+++ incidencesmanagement/core/views.py
@@ -1,6 +1,22 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
 from django.views.generic import TemplateView
+from .models import UserEmployee
 
 
 class HomeView(TemplateView):
+    """Home page view"""
     template_name = 'core/index.html'
+
+
+class UserListView(ListView):
+    """Displays all users"""
+    model = UserEmployee
+    fields = ["username"
+              "email",
+              "first_name",
+              "last_name",
+              "department",
+              "last_login",
+              "date_joined"]
+
