26dfd1d4d46e9d5a2367fba9604942ce6de34eb3
Stevens Brito <stevensbrito@gmail.com>
2018-10-20 04:45:49 -0400

    Add User employee creation view

--- incidencesmanagement/core/urls.py
+++ incidencesmanagement/core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from .views import HomeView, UserListView
+from .views import HomeView, UserListView, UserCreate
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
     path('users/', UserListView.as_view(), name='core_users_list'),
-    # path('', HomeView.as_view(), name='home'),
+    path('users/create', UserCreate.as_view(), name='core_user_create'),
 ]
